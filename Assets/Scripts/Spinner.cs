82aa83d22db688e2ab743b22d4ce6f1ed98e8986
Alejitaso <alzatealejandro20@gmail.com>
2026-04-30 11:31:46 -0500

    fix issue of speed to spinner timedeltatime

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -7,9 +7,11 @@ public class Spinner : MonoBehaviour
     [SerializeField] float yAngle = 1f;
 
     [SerializeField] float zAngle = 0f;
+
+    [SerializeField] float speed = 1f;
     
     void Update()
     {
-        transform.Rotate(xAngle, yAngle, zAngle);
+        transform.Rotate(xAngle * speed * Time.deltaTime, yAngle * speed * Time.deltaTime, zAngle * speed * Time.deltaTime);
     }
 }
